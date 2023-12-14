a16871bdad1bd5469da52637b54ead60f47ef0a9
Evgeniy.Delfoncev <devgen9275@gmail.com>
2023-12-14 20:08:28 +0300

    Update

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,27 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.models.Car;
 import web.service.CarService;
-import java.util.List;
 
 @Controller
 public class CarController {
 
+
+    private final CarService carService;
+
     @Autowired
-    private CarService carService;
+    private CarController (CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> cars;
-
-        if (count != null && count < 5) {
-            cars = carService.getCars(count);
-        } else {
-            cars = carService.getAllCars();
-        }
-
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,11 +1,13 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.models.Car;
-
 import java.util.List;
 
+@Component
 public interface CarService {
-    List<Car> getCars(int count);
+    List<Car> getCars(Integer count);
 
     List<Car> getAllCars();
+
 }
